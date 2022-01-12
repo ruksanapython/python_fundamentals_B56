25759d5a1cbcc50db6bed057223aa90328c22605
ruksanapython <97605119+ruksanapython@users.noreply.github.com>
2022-01-13 00:21:50 +0530

    Add files via upload
    First file

---
+++ python-fundamentals.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+Firstname = 'SyedRuksana'
+print(Firstname)
+
+
+# In[4]:
+
+
+firstname = "Syedruksana"
+print(firstname)
+
+
+# In[ ]:
+
+
+# Rules for declaring the variables in python:
+
+
+# In[ ]:
+
+
+1.No name space while declaring the variables
+
+
+# In[5]:
+
+
+first name = "syed"
+print(first name)
+
+
+# In[6]:
+
+
+firstname = "Syed"
+print(firstname)
+
+
+# In[ ]:
+
+
+2. variable name should not start with a number ***
+
+
+# In[7]:
+
+
+1alpha = "Beta"
+print(1alpha)
+
+
+# In[9]:
+
+
+alpha1 = "Beta"
+print(alpha1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+3.A variable name should not contain special character
+
+
+# In[11]:
+
+
+name$ = "Alpha"
+print(name$)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#commenting the code in python
+# ----> is used to commenting the lines
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
