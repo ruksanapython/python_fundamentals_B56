648d9c57b59bf001f0e65f7b979cf1a0c92da376
ruksanapython <97605119+ruksanapython@users.noreply.github.com>
2022-01-16 23:50:35 +0530

    Add files via upload

---
+++ python class3-datatypes.py
@@ -0,0 +1,381 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Introduction to list data types
+
+
+# In[ ]:
+
+
+listdatatypes: it is collection of items declared in a perticular order.
+    Classification : It is classified as an mutable datatypes
+        how to define the list .....?
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+students = ['Ayaan','Rehaan','Armaan','suhail']
+
+
+# In[1]:
+
+
+print(students)
+
+
+# In[3]:
+
+
+students = ['Ayaan','Rehaan','Armaan','suhail']
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+type(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#req : i want to access Armaan name from the above list....?
+
+
+# In[ ]:
+
+
+Introduction to indexing : 0,1,2,3,4....n
+
+
+# In[6]:
+
+
+print(students[2])
+
+
+# In[ ]:
+
+
+# req : i want to access suhail name from the above list
+
+
+# In[7]:
+
+
+print(students[3])
+
+
+# In[ ]:
+
+
+# how to add Karim name in the above the list
+
+
+# In[11]:
+
+
+print(students)
+
+
+# In[12]:
+
+
+students.append('Rumana')
+
+
+# In[13]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+# i want to add lukmaa to second index
+
+
+# In[ ]:
+
+
+students.insert(2,'lukmaan')
+
+
+# In[15]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+# i want to add lukmaan in second index
+
+
+# In[ ]:
+
+
+students.insert(2,'lukmaan')
+
+
+# In[17]:
+
+
+print(students)
+
+
+# In[18]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[20]:
+
+
+students.insert{2,'lukmaan'}
+
+
+# In[21]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# i want to add Nayeem to the 2nd index list
+
+
+# In[ ]:
+
+
+students.insert[2,'Nayeem']
+
+
+# In[22]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+students.insert(3,'Nayeem')
+
+
+# In[23]:
+
+
+print(students)
+
+
+# In[24]:
+
+
+students.insert(3,'Nayeem')
+print(students)
+
+
+# In[25]:
+
+
+print(students[3])
+
+
+# In[26]:
+
+
+print(students[5])
+
+
+# In[36]:
+
+
+students[5] = 'Rizwana'
+print(students)
+
+
+# In[37]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+# i want to del karim from the list
+
+
+# In[ ]:
+
+
+del students[5]
+
+
+# In[30]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+# i want replace karim with Rizwana
+
+
+# In[31]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+# i want to replace the Rizwana with Nagma
+
+
+# In[ ]:
+
+
+students[5] = 'Nagma'
+
+
+# In[32]:
+
+
+print(students)
+
+
+# In[33]:
+
+
+print(students[5])
+
+
+# In[ ]:
+
+
+students[5] = 'Nagma'
+
+
+# In[34]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[35]:
+
+
+students[5] = 'Nagma'
+print(students)
+
+
+# In[38]:
+
+
+print(students[6])
+
+
+# In[ ]:
+
+
+
+
+
+# In[39]:
+
+
+print(students)
+
+
+# In[41]:
+
+
+students[6] = 'lukmaan'
+print(students)
+
+
+# In[42]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
